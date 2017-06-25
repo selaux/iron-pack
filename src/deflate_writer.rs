5e7834a996ca0f180fe6bf57da5ca93ddd4674d6
Stefan Lau <github@stefanlau.com>
2017-06-25 15:16:33 +0200

    Handle errors during compression

--- src/deflate_writer.rs
+++ src/deflate_writer.rs
@@ -1,18 +1,21 @@
+use std::io;
 use iron::prelude::*;
 use iron::headers::*;
 use iron::modifier::Modifier;
 use libflate::deflate;
 
+fn stringify_err(err: io::Error) -> String { format!("Error compressing body: {}", err) }
+
 pub struct DeflateWriter;
 
 impl DeflateWriter {
-    fn get_compressed_body(&self, res: &mut Response) -> Option<Vec<u8>> {
+    fn get_compressed_body(&self, res: &mut Response) -> Result<Vec<u8>, String> {
         if let Some(ref mut body) = res.body {
             let mut encoder = deflate::Encoder::new(Vec::new());
-            body.write_body(&mut encoder).unwrap();
-            return Some(encoder.finish().into_result().unwrap());
+            body.write_body(&mut encoder).map_err(stringify_err)?;
+            return encoder.finish().into_result().map_err(stringify_err);
         } else {
-            None
+            Err(String::from("Error compressing body: No response body present."))
         }
     }
 }
@@ -21,7 +24,7 @@ impl Modifier<Response> for DeflateWriter {
     fn modify(self, mut res: &mut Response) {
         let compressed = self.get_compressed_body(&mut res);
 
-        if let Some(compressed_bytes) = compressed {
+        if let Ok(compressed_bytes) = compressed {
             res.headers.set(ContentEncoding(vec![Encoding::Deflate]));
             compressed_bytes.modify(res);
         }
