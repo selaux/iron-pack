59bcef720947560fc20e930a39b3f72b4c0a0cef
Stefan Lau <github@stefanlau.com>
2017-06-25 14:42:04 +0200

    Weigh by quality

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,12 @@ fn which_compression(req: &Request, res: &Response) -> Option<Encoding> {
 
     {
         if let Some(&AcceptEncoding(ref quality_items)) = req.headers.get::<AcceptEncoding>() {
-            let allowed_qi = quality_items.iter().find(|qi| qi.item == Encoding::Gzip || qi.item == Encoding::Deflate);
+            let mut sorted_qis = quality_items.clone();
+
+            sorted_qis.sort_by(|qi1, qi2| qi2.quality.cmp(&(qi1.quality)));
+
+
+             let allowed_qi = sorted_qis.iter().find(|qi| qi.item == Encoding::Gzip || qi.item == Encoding::Deflate);
 
             if let Some(&QualityItem { item: ref encoding, quality: _ }) = allowed_qi {
                 return Some(encoding.clone());
@@ -230,6 +235,75 @@ mod deflate_tests {
 
         headers.set(
             AcceptEncoding(vec![qitem(Encoding::Deflate)])
+        );
+        let res = request::post("http://localhost:3000/",
+                                headers,
+                                &value,
+                                &chain).unwrap();
+
+        {
+            assert_eq!(res.headers.get::<ContentEncoding>(), Some(&ContentEncoding(vec![Encoding::Deflate])));
+        }
+
+        let compressed_bytes = response::extract_body_to_bytes(res);
+        let mut decoder = deflate::Decoder::new(&compressed_bytes[..]);
+        let mut decoded_data = Vec::new();
+        decoder.read_to_end(&mut decoded_data).unwrap();
+        assert_eq!(decoded_data, value.into_bytes());
+    }
+}
+
+#[cfg(test)]
+mod priority_tests {
+    extern crate iron_test;
+
+    use std::io::Read;
+    use iron::headers::*;
+    use iron::Headers;
+    use self::iron_test::{request, response};
+    use libflate::deflate;
+
+    use super::test_common::*;
+
+    #[test]
+    fn it_should_use_the_more_prior_algorithm() {
+        let mut headers = Headers::new();
+        let value = "a".repeat(1000);
+        let chain = build_compressed_echo_chain(false);
+
+        headers.set(
+            AcceptEncoding(vec![
+                QualityItem { item: Encoding::Gzip, quality: q(0.5) },
+                QualityItem { item: Encoding::Deflate, quality: q(1.0) }
+            ])
+        );
+        let res = request::post("http://localhost:3000/",
+                                headers,
+                                &value,
+                                &chain).unwrap();
+
+        {
+            assert_eq!(res.headers.get::<ContentEncoding>(), Some(&ContentEncoding(vec![Encoding::Deflate])));
+        }
+
+        let compressed_bytes = response::extract_body_to_bytes(res);
+        let mut decoder = deflate::Decoder::new(&compressed_bytes[..]);
+        let mut decoded_data = Vec::new();
+        decoder.read_to_end(&mut decoded_data).unwrap();
+        assert_eq!(decoded_data, value.into_bytes());
+    }
+
+    #[test]
+    fn it_should_use_the_more_prior_algorithm_2() {
+        let mut headers = Headers::new();
+        let value = "a".repeat(1000);
+        let chain = build_compressed_echo_chain(false);
+
+        headers.set(
+            AcceptEncoding(vec![
+                QualityItem { item: Encoding::Deflate, quality: q(1.0) },
+                QualityItem { item: Encoding::Gzip, quality: q(0.5) }
+            ])
         );
         let res = request::post("http://localhost:3000/",
                                 headers,
